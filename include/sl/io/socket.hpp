e537f36af9c7a64969835973db154dead9d1509c
UsatiyNyan <us4tiyny4n@gmail.com>
2025-01-03 20:32:57 +0100

    io: socket: refactoring

--- include/sl/io/socket.hpp
+++ include/sl/io/socket.hpp
@@ -5,11 +5,11 @@
 #pragma once
 
 #include "sl/io/file.hpp"
+#include "sl/io/result.hpp"
+
+#include <sl/meta/func/unit.hpp>
 
 #include <netinet/in.h>
-#include <tl/expected.hpp>
-
-#include <system_error>
 
 namespace sl::io {
 
@@ -20,18 +20,15 @@ struct socket {
     struct connection;
 
 public:
-    [[nodiscard]] static tl::expected<socket, std::error_code>
-        create(std::int32_t domain, std::int32_t type, std::int32_t protocol);
+    [[nodiscard]] static result<socket> create(std::int32_t domain, std::int32_t type, std::int32_t protocol);
 
-    [[nodiscard]] tl::expected<tl::monostate, std::error_code>
-        set_opt(std::int32_t level, std::int32_t name, std::int32_t opt);
-    [[nodiscard]] tl::expected<std::int32_t, std::error_code> get_opt(std::int32_t level, std::int32_t name);
+    [[nodiscard]] result<meta::unit> set_opt(std::int32_t level, std::int32_t name, std::int32_t opt);
+    [[nodiscard]] result<std::int32_t> get_opt(std::int32_t level, std::int32_t name);
 
-    [[nodiscard]] tl::expected<tl::monostate, std::error_code> set_blocking(bool is_blocking);
-    [[nodiscard]] tl::expected<bool, std::error_code> get_blocking();
+    [[nodiscard]] result<meta::unit> set_blocking(bool is_blocking);
+    [[nodiscard]] result<bool> get_blocking();
 
-    [[nodiscard]] tl::expected<bound_server, std::error_code>
-        bind(std::uint16_t family, std::uint16_t port, std::uint32_t in_addr) &&;
+    [[nodiscard]] result<bound_server> bind(std::uint16_t family, std::uint16_t port, std::uint32_t in_addr) &&;
 
 public:
     file handle;
@@ -39,14 +36,14 @@ public:
 
 struct socket::bound_server {
     // backlog should be more than 0 and it will be truncated if exceeds 4096 by system
-    [[nodiscard]] tl::expected<listening_server, std::error_code> listen(std::uint16_t backlog) &&;
+    [[nodiscard]] result<listening_server> listen(std::uint16_t backlog) &&;
 
 public:
     socket socket;
 };
 
 struct socket::listening_server {
-    [[nodiscard]] tl::expected<socket::connection, std::error_code> accept();
+    [[nodiscard]] result<socket::connection> accept();
 
 public:
     socket socket;
